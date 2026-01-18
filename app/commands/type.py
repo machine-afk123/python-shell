934c09aaa183fd0b69026049d65d00259800e807
machine-afk123 <serraojoshua@gmail.com>
2026-01-18 20:23:42 +0800

    (feat) type command can now check if command has an executable

--- app/commands/type.py
+++ app/commands/type.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 builtins = set()
 
@@ -8,11 +9,20 @@ with open(data) as f:
     for command in f:
         builtins.add(command.strip())
 
+def get_executable(command: str) -> str:
+    paths = os.getenv("PATH", "").split(os.pathsep)
+    for path in paths:
+        fullPath = os.path.join(path, command)
+        if os.access(fullPath, os.X_OK):
+            return fullPath
+
+    return ""
+
 def check_command(*args: str) -> None: 
     for command in args:
         if command in builtins:
             print(f"{command} is a shell builtin")
+        elif (executable := get_executable(command)):
+            print(f"{command} is {executable}")
         else:
             print(f"{command}: not found")
-    
-    return
