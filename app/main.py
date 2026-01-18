01044f75ae294d110edd2338734dd24b9fe75d60
machine-afk123 <serraojoshua@gmail.com>
2026-01-18 17:39:19 +0800

    Add REPL loop

--- app/main.py
+++ app/main.py
@@ -2,13 +2,11 @@ import sys
 
 
 def main():
-    # while True:
-    sys.stdout.write("$ ")
-    usr_input = input()
-    if usr_input:
-        print(f"{usr_input}: command not found")
-    pass
-
+    while True:
+        sys.stdout.write("$ ")
+        usr_input = input()
+        if usr_input:
+            print(f"{usr_input}: command not found")
 
 if __name__ == "__main__":
     main()
