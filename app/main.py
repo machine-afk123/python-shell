c2195c4332852599ea767963164d3d3e5435e154
machine-afk123 <serraojoshua@gmail.com>
2026-01-18 17:54:30 +0800

    Add initial functionality for echo command

--- app/main.py
+++ app/main.py
@@ -5,9 +5,16 @@ def main():
     while True:
         sys.stdout.write("$ ")
         usr_input = input()
-        match usr_input:
+        # for every arg we want the first to be the command
+        usr_input_args = usr_input.split()
+        command = usr_input_args[0]
+        match command:
             case "exit":
                 break
+            case "echo":
+                # if usr_input_args[1].startswith("-"):
+                #     continue
+                print(" ".join(usr_input_args[1:]))
             case "":
                 continue
             case _:
