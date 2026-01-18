dc1d58ed7345a4f10ce7db176716e458b09604dc
machine-afk123 <serraojoshua@gmail.com>
2026-01-18 17:42:40 +0800

    Add exit command functionality to break loop

--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
         usr_input = input()
-        if usr_input:
-            print(f"{usr_input}: command not found")
+        match usr_input:
+            case "exit":
+                break
+            case "":
+                continue
+            case _:
+                print(f"{usr_input}: command not found")
 
 if __name__ == "__main__":
     main()
