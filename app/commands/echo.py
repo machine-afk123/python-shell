32bb93de9a549d6af7a0ac946578f64f04b0bb87
machine-afk123 <serraojoshua@gmail.com>
2026-01-25 20:17:57 +0800

    (fix) use shlex instead to parse string

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -1,9 +1,16 @@
 import re
 import sys
+import shlex
 
 def echo(input: str) -> None:
-    input = input.replace("echo", "")
-    pattern = r"'+|\"+|[\[\]]"
-    splitted = re.split(pattern, input)
-    splitted = [s.strip() for s in splitted if s]
-    sys.stdout.write("".join(splitted) + "\n")
+    input = input.replace("echo", "", 1)
+    lexer = shlex.shlex(input, posix=True)
+    lexer.whitespace_split = True
+    lexer.whitespace = " \t\r\n"
+    lexer.quotes = "'\""
+
+    tokens = list(lexer)
+
+    cleaned = " ".join(tokens)
+    sys.stdout.write(cleaned + "\n")
+    
