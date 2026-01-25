5d09cb6422ede154b77fa4ac2e5f864636686f18
machine-afk123 <serraojoshua@gmail.com>
2026-01-25 18:45:29 +0800

    (feat) add support for ~ in cd

--- app/commands/cd.py
+++ app/commands/cd.py
@@ -2,9 +2,12 @@ import os
 from pathlib import Path
 
 def cd(path: str) -> None:
-    new_path = Path(path)
+    path = path.strip()
+    if path == "~":
+        path = os.getenv("HOME")
+
+    new_path = Path(path).resolve()
     if new_path.is_dir() and new_path.exists():
         os.chdir(new_path)
     else:
         print(f"cd: {path}: No such file or directory")
-        
