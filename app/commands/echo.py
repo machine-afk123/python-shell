91037b89742fc52abe01029911e6acb36693d867
machine-afk123 <serraojoshua@gmail.com>
2026-01-25 19:51:29 +0800

    (fix) minor space fix

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -5,4 +5,5 @@ def echo(input: str) -> None:
     input = input.replace("echo", "")
     pattern = r"'+|\"+|[\[\]]"
     splitted = re.split(pattern, input)
+    splitted = [s.strip() for s in splitted if s]
     sys.stdout.write("".join(splitted) + "\n")
