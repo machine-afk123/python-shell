fac7af7279b5a783047eec68cd2c8ab122e3cfa2
Joshua <serraojoshua@gmail.com>
2026-01-27 18:01:58 +0800

    (refactor) Refactor builtins file in load_builtin and use lru_cache for lazy loading of builtins commands

--- app/commands/type.py
+++ app/commands/type.py
@@ -1,16 +1,9 @@
 from pathlib import Path
-import os
 from app.commands.execute import get_executable
-
-builtins = set()
-
-data = Path(__file__).resolve().parent.parent / "data" / "builtins.txt"
-
-with open(data) as f:
-    for command in f:
-        builtins.add(command.strip())
+from app.utilities.load_builtin import get_builtins
 
 def check_command(*args: str) -> None: 
+    builtins = get_builtins()
     for command in args:
         if command in builtins:
             print(f"{command} is a shell builtin")
---
+++ app/utilities/load_builtin.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from functools import lru_cache
+import os
+
+data = Path(__file__).resolve().parent.parent / "data" / "builtins.txt"
+
+# lazy load the builtins file
+def get_builtins() -> None:
+    mtime = os.path.getmtime(data)
+    return load_builtins(mtime)
+
+@lru_cache(maxsize=1)
+def load_builtins(mtime: float) -> set[str]:
+    builtins = set()
+
+    with open(data) as f:
+        for command in f:
+            builtins.add(command.strip())
+                        
+    return builtins  
